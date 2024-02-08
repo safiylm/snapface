8c9ebfac79b6dc0d1ba14bab8385ae729ef212dc
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-09 00:20:55 +0100

    improve the display of text-snap components

--- src/app/text-snap-list/text-snap-list.component.ts
+++ src/app/text-snap-list/text-snap-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
 
+import { Component , OnInit} from '@angular/core';
+import { TextSnap } from '../models/text-snap.model';
+import { TextSnapsService } from '../services/text-snap-service'
+
+//...
 @Component({
   selector: 'app-text-snap-list',
   templateUrl: './text-snap-list.component.html',
   styleUrls: ['./text-snap-list.component.scss']
 })
-export class TextSnapListComponent {
 
+export class TextSnapListComponent implements OnInit {
+constructor(private TextSnapsService: TextSnapsService) { }
+
+  textSnaps!: TextSnap[];
+
+  ngOnInit(): void {
+    this.textSnaps = this.TextSnapsService.TextSnaps;
+}
 }
