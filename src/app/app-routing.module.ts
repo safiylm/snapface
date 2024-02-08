cb05c89a3047c4561744fd395ab33207778fd928
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-09 00:22:34 +0100

    code of routing & nav = header components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component'
+import { TextSnapListComponent } from './text-snap-list/text-snap-list.component'
+import { HomeComponent } from './home/home.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'snaps/face', component: FaceSnapListComponent },
+  { path: 'snaps/text', component: TextSnapListComponent  },
+  { path: '', component:  HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
