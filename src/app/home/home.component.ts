42b0e7f1ea0c56466c9e21eb4a5a40a3a0da2574
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-11 15:05:10 +0100

    add 'users.json' and itere user in home page in console.log

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as  users from '../json-database/users.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent  implements OnInit {
+  title = 'json-read-example';
+  data: any = users;
+
+  ngOnInit() {
+    console.log('Data', this.data);
+  }
 }
