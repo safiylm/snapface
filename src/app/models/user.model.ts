73445d1c1908b65f752204f4120bb8ea5eb9ee68
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-12 14:11:20 +0100

    Create user.model.ts

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public id:number,
+        public image: string,
+        public name: string,
+        public email: string,
+        public gender: string,
+       ) {
+    }
+}
