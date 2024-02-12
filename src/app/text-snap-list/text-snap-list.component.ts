ca60954240ca30182cc4bb71a493bc32f9ba223d
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-12 21:24:54 +0100

    display photos profil & background from url saving in users array

--- src/app/text-snap-list/text-snap-list.component.ts
+++ src/app/text-snap-list/text-snap-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit  } from '@angular/core';
 import { TextSnap } from '../models/text-snap.model';
 import { TextSnapsService } from '../services/text-snap-service'
 import { ActivatedRoute, ParamMap } from '@angular/router'
+import { user_array } from '../json-database/users-array';
 
 //...
 @Component({
@@ -15,10 +16,19 @@ export class TextSnapListComponent implements OnInit {
 
   constructor(private route: ActivatedRoute , private TextSnapsService: TextSnapsService) { }
   id !:  any;
+  photo_profil !: string
+  photo_background !: string
   textSnaps!: TextSnap[];
   
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
+
+    user_array.forEach((item) => {
+      if( item.id == this.id){
+        this.photo_profil= item['photos-profil'];
+        this.photo_background= item['photos-background'];
+      }
+    })
     this.textSnaps = this.TextSnapsService.TextSnaps;
   }
   
