c9f0575c19634b1712821fe46d890330d3e75c0e
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-12 23:55:18 +0100

    display in user account only his facesnap & textsnap

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snap-service'
 import { ActivatedRoute, ParamMap } from '@angular/router'
-import { user_array } from '../json-database/users-array';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -15,21 +14,12 @@ export class FaceSnapListComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private faceSnapsService: FaceSnapsService) { }
   id !: any;
-  photo_profil !: string
-  photo_background !: string
   faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
 
-    user_array.forEach((item) => {
-      if( item.id == this.id){
-        this.photo_profil= item['photos-profil'];
-        this.photo_background= item['photos-background'];
-      }
-    })
-
-    this.faceSnaps = this.faceSnapsService.faceSnaps;
+    this.faceSnaps = this.faceSnapsService.getAllFaceSnapsByAuteur(this.id);
   }
 
 }
--- src/app/text-snap-list/text-snap-list.component.ts
+++ src/app/text-snap-list/text-snap-list.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit  } from '@angular/core';
 import { TextSnap } from '../models/text-snap.model';
 import { TextSnapsService } from '../services/text-snap-service'
 import { ActivatedRoute, ParamMap } from '@angular/router'
-import { user_array } from '../json-database/users-array';
 
 //...
 @Component({
@@ -16,20 +15,12 @@ export class TextSnapListComponent implements OnInit {
 
   constructor(private route: ActivatedRoute , private TextSnapsService: TextSnapsService) { }
   id !:  any;
-  photo_profil !: string
-  photo_background !: string
   textSnaps!: TextSnap[];
   
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
 
-    user_array.forEach((item) => {
-      if( item.id == this.id){
-        this.photo_profil= item['photos-profil'];
-        this.photo_background= item['photos-background'];
-      }
-    })
-    this.textSnaps = this.TextSnapsService.TextSnaps;
+    this.textSnaps = this.TextSnapsService.getTextSnapByAuteur(this.id);
   }
   
 
