0bc6564fb0a24e1b18afd619d2bbb3488c232c3a
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-12 19:55:09 +0100

    Update users-array.ts

--- src/app/json-database/users-array.ts
+++ src/app/json-database/users-array.ts
@@ -2,44 +2,29 @@ export let user_array = [
 
     {
         "id": 1,
+        "photos-profil": "https://images.pexels.com/photos/1550618/pexels-photo-1550618.jpeg",
+        "photos-background": "https://images.pexels.com/photos/4767578/pexels-photo-4767578.jpeg",
+
         "name": "Luca",
         "email": "luca@gmail.com",
         "gender": "male"
     },
     {
         "id": 2,
+        "photos-profil": "https://images.pexels.com/photos/17542964/pexels-photo-17542964/free-photo-of-lumineux-route-lunettes-de-soleil-gens.jpeg",
+        "photos-background": "https://images.pexels.com/photos/4767578/pexels-photo-4767578.jpeg",
         "name": "Lilly",
         "email": "lilly@gmail.com",
         "gender": "female"
     },
     {
         "id": 3,
+        "photos-profil": "https://images.pexels.com/photos/20225732/pexels-photo-20225732.jpeg",
+        "photos-background": "https://images.pexels.com/photos/4767578/pexels-photo-4767578.jpeg",
+
         "name": "Anna",
         "email": "anna@gmail.com",
         "gender": "female"
     },
-    {
-        "id": 4,
-        "name": "John",
-        "email": "john@gmail.com",
-        "gender": "male"
-    },
-    {
-        "id": 5,
-        "name": "Mary",
-        "email": "mary@gmail.com",
-        "gender": "female"
-    },
-    {  
-        "id": 6,  
-        "name": "Mary",  
-        "email": "mary@gmail.com",  
-        "gender": "female"  
-      }  ,
-      {  
-        "id": 6,  
-        "name": "Mary",  
-        "email": "mary@gmail.com",  
-        "gender": "female"  
-      }  
+   
 ]
