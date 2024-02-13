5ba780203f74d151d77ac5933b09771cf3cbc94d
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-13 17:03:54 +0100

    Update face-snap-service.ts

--- src/app/services/face-snap-service.ts
+++ src/app/services/face-snap-service.ts
@@ -6,54 +6,57 @@ import { FaceSnap } from '../models/face-snap.model'
 })
 export class FaceSnapsService {
   faceSnaps: FaceSnap[] = [
-        new FaceSnap( 1 ,'cafe', 'J\'adore le café', 'https://live.staticflickr.com/47/150654741_ae02588670_b.jpg', new Date, 5, 2),
-       
-        new FaceSnap( 2 ,"Nature", "J'adore la nature ",
-          "https://img.freepik.com/photos-gratuite/champ-lavande-au-coucher-du-soleil-pres-valensole_268835-3910.jpg?size=626&ext=jpg&ga=GA1.2.337367146.1690124945&semt=sph",
-          new Date, 16445, 1),
-          new FaceSnap( 3 ,"Architecture", "Une architecture incroyable ",
-          "https://images.pexels.com/photos/18250922/pexels-photo-18250922/free-photo-of-building-mur-coin-histoire.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
-          new Date, 16445, 3),
-    
+    new FaceSnap(1, 'cafe', 'J\'adore le café', 'https://live.staticflickr.com/47/150654741_ae02588670_b.jpg', new Date, 5, 2),
+    new FaceSnap(2, "Nature", "J'adore la nature ",
+      "https://img.freepik.com/photos-gratuite/champ-lavande-au-coucher-du-soleil-pres-valensole_268835-3910.jpg?size=626&ext=jpg&ga=GA1.2.337367146.1690124945&semt=sph",
+      new Date, 16445, 1),
+    new FaceSnap(3, "Architecture", "Une architecture incroyable ",
+      "https://images.pexels.com/photos/18250922/pexels-photo-18250922/free-photo-of-building-mur-coin-histoire.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+      new Date, 16445, 3),
+
   ];
 
 
   getAllFaceSnaps(): FaceSnap[] {
     return this.faceSnaps;
-}
+  }
 
-getAllFaceSnapsByAuteur( userId: number ): FaceSnap[] {
+  pushNewFaceSnap(fs: FaceSnap): void {
+    this.faceSnaps.push(fs);
+  }
 
-  let faceSnap = this.faceSnaps.filter(faceSnap => faceSnap.userId == userId);
+  getAllFaceSnapsByAuteur(userId: number): FaceSnap[] {
 
-  if (!faceSnap) {
+    let faceSnap = this.faceSnaps.filter(faceSnap => faceSnap.userId == userId);
+
+    if (!faceSnap) {
       throw new Error('FaceSnap not found!');
-  } 
-  return faceSnap;
-}
+    }
+    return faceSnap;
+  }
 
-getFaceSnapById(faceSnapId: number): FaceSnap {
-  const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
-  if (!faceSnap) {
+  getFaceSnapById(faceSnapId: number): FaceSnap {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+    if (!faceSnap) {
       throw new Error('FaceSnap not found!');
-  } else {
+    } else {
       return faceSnap;
+    }
   }
-} 
 
 
-snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
-  const faceSnap = this.getFaceSnapById(faceSnapId);
-  snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
-}
+  snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
+    const faceSnap = this.getFaceSnapById(faceSnapId);
+    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+  }
 
-unsnapFaceSnapById(faceSnapId: number): void {
-  const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
-  if (faceSnap) {
+  unsnapFaceSnapById(faceSnapId: number): void {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+    if (faceSnap) {
       faceSnap.snaps--;
-  } else {
+    } else {
       throw new Error('FaceSnap not found!');
+    }
   }
-}
 
 }
