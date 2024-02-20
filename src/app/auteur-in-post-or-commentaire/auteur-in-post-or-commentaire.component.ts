067765a4cb09046fb4e974ac69c83e99cdfc3e08
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-20 08:50:25 +0100

    add auteur in post or comment

---
+++ src/app/auteur-in-post-or-commentaire/auteur-in-post-or-commentaire.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {User} from '../../models/user.model';
+import {UserService} from '../../services/user-service';
+
+@Component({
+  selector: 'app-auteur-in-post-or-commentaire',
+  templateUrl: './auteur-in-post-or-commentaire.component.html',
+  styleUrls: ['./auteur-in-post-or-commentaire.component.scss']
+})
+export class AuteurInPostOrCommentaireComponent  implements OnInit {
+ 
+  //Passing Data into this Component
+  @Input() id !: string ;
+ 
+  user !: User;
+
+  constructor(private UserService: UserService) { }
+
+
+  retrieveUser(): void {
+    this.UserService.getUser( this.id) 
+      .subscribe({
+        next: (data) => {
+          this.user = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  ngOnInit() {
+   this.retrieveUser()
+  }
+  
+}
