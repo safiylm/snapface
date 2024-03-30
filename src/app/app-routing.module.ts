561efa0dd439415797825d223100666d53153a2f
safiylm <121391404+safiylm@users.noreply.github.com>
2024-03-30 14:02:24 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { HomeComponent } from './home/home.component'
 import {  AuthConnexionUserComponent } from './auth-connexion-user/auth-connexion-user.component'
 import { AuthInscriptionUserComponent } from './auth-inscription-user/auth-inscription-user.component'
 import { UserAccountComponent } from './user-account/user-account.component'
-import { UserDataUpdateComponent } from './user-data-update/user-data-update.component'
-import { PublicationCreateComponent } from './publication-create/publication-create.component';
 import { PublicationEditComponent } from './publication-edit/publication-edit.component';
+import { AuthGuard } from './guards/auth.guard';  
+import { MyAccountComponent } from './my-account/my-account.component';
+
 const routes: Routes = [
   { path: 'user/:id', component: UserAccountComponent  },
   { path: '', component:  HomeComponent },
   { path: 'connexion', component:  AuthConnexionUserComponent },
   { path: 'inscription', component:  AuthInscriptionUserComponent },
-  { path: 'user/data/update', component:  UserDataUpdateComponent },
-  { path: 'publication/creation', component:  PublicationCreateComponent },
+  { path: 'mon-compte', component:  MyAccountComponent, canActivate : [AuthGuard] },
   { path: 'publication/edit/:id', component:  PublicationEditComponent },
 
 ];
