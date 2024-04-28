bd70ee46a21adf79bfa6796bb975ccfe45a049be
safiylm <121391404+safiylm@users.noreply.github.com>
2024-04-28 22:41:58 +0200

    Update db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URL
-const url = 'mongodb://localhost/facesnap';
+//const url = 'mongodb://localhost/facesnap';
+const url ="mongodb+srv://safi:safinaz@cluster0.kwh9z4l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/facesnap"
 const client = new MongoClient(url);
 // Database Name
 const dbName = 'facesnap';
