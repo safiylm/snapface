117c2f14fd11978e816ee55cd8ce950a04b20de3
safiylm <121391404+safiylm@users.noreply.github.com>
2024-06-22 20:00:05 +0200

    set header for Cors problem

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -111,10 +111,19 @@ exports.delete = async (req, res) => {
 
 // Retrieve all Users from the database.
 exports.findAll = async (req, res) => {
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin, Accept');        
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://snapface.onrender.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   const findResult = await collection_user.find({}).toArray();
   res.send(findResult);
 
