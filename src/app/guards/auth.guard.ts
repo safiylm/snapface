f9442a9983af7c0022a6ac22746f10636d1fc2a6
safiylm <121391404+safiylm@users.noreply.github.com>
2024-07-20 00:52:17 +0200

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,30 +1,33 @@
-import { Injectable } from '@angular/core';      
-import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';      
-import { Observable } from 'rxjs';      
-@Injectable({      
-   providedIn: 'root'      
-})      
-export class AuthGuard implements CanActivate {      
-   constructor(private router: Router) { }      
-   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {      
-      if (this.isLoggedIn()) {      
-      return true;      
-      }      
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+@Injectable({
+   providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+   constructor(private router: Router) { }
+
+   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+      if (this.isLoggedIn()) {
+         return true;
+      }
       // navigate to login page as user is not authenticated      
-   this.router.navigate(['/login']);      
-return false;      
-}      
-public isLoggedIn(): boolean {      
-   let status = false;      
-   if (localStorage.getItem('isLoggedIn') == "true") {      
-      status = true;      
+      this.router.navigate(['/connexion']);
+      return false;
    }
-     else {      
-      status = false;      
-      }      
-   return status;      
-   }    
-}    
+
+   public isLoggedIn(): boolean {
+      let status = false;
+      if (localStorage.getItem('isLoggedIn') == "true") {
+         status = true;
+      }
+      else {
+         status = false;
+      }
+      return status;
+   }
+}
 
 
 // import { CanActivateFn } from '@angular/router';
