7a40d36bd6de5ecf6c6521711c2774d870d87ec9
safiylm <121391404+safiylm@users.noreply.github.com>
2024-07-24 15:45:03 +0200

    changement in route (add new children route)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,41 @@ import { AuthConnexionUserComponent } from './auth-connexion-user/auth-connexion
 import { AuthInscriptionUserComponent } from './auth-inscription-user/auth-inscription-user.component'
 import { UserAccountComponent } from './user-account/user-account.component'
 import { PublicationEditComponent } from './publication-edit/publication-edit.component';
+import { UserDataUpdateComponent } from './user-data-update/user-data-update.component';
+import { PublicationCreateComponent } from './publication-create/publication-create.component';
 import { AuthGuard } from './guards/auth.guard';  
 import { MyAccountComponent } from './my-account/my-account.component';
+import { FormulaireGuard } from './guards/fomulaire.guard';
 
 const routes: Routes = [
-  { path: 'user/:id', component: UserAccountComponent  },
+  { path: 'user/:id', component: UserAccountComponent },
   { path: '', component:  HomeComponent },
   { path: 'connexion', component:  AuthConnexionUserComponent },
-  { path: 'inscription', component:  AuthInscriptionUserComponent },
-  { path: 'mon-compte', component:  MyAccountComponent, canActivate : [AuthGuard] },
-  { path: 'publication/edit/:id', component:  PublicationEditComponent },
+  { path: 'inscription', component:  AuthInscriptionUserComponent ,
+    canDeactivate : [FormulaireGuard]
+
+  },
+  { path: 'mon-compte', component:  MyAccountComponent, canActivate : [AuthGuard] ,
+  children: [
+    {
+      path: 'edit',  // child route path
+      title: "Edit data",
+      component: UserDataUpdateComponent,  // child route component that the router renders
+      canDeactivate : [FormulaireGuard]
+    },
+    {
+      path: 'create-new-post',  // child route path
+      title: "Create new post",
+      component: PublicationCreateComponent,  // child route component that the router renders
+      canDeactivate : [FormulaireGuard]
+
+    },
+  ]
+  },
+  { path: 'publication/edit/:id', component:  PublicationEditComponent,
+    canDeactivate : [FormulaireGuard]
+
+   },
 
 ];
 
--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { UserService } from '../../services/user-service'
 import { User } from '../../models/user.model'
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-account',
@@ -17,36 +17,45 @@ export class MyAccountComponent implements OnInit {
   isDisplayCreateNewPost: boolean = false;
   isDisplayUserEditForm: boolean = false;
 
-  constructor(private userService: UserService, private route: ActivatedRoute) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) { }
 
   data?: User;
 
   ngOnInit() {
     this.id = localStorage.getItem('userId')?.toString() as string;
-
+    if(this.router.url == "/mon-compte/edit"){
+      this.displayUserEditForm()
+    }
+    if(this.router.url == "/mon-compte/create-new-post"){
+      this.displayCreateNewPost()
+    }
   }
 
   displayPosts() {
     this.isDisplayPosts = true;
     this.isDisplayCreateNewPost = false;
     this.isDisplayUserEditForm = false;
+    this.router.navigate(['/mon-compte']);
+
   }
 
   displayCreateNewPost() {
     this.isDisplayPosts = false;
     this.isDisplayCreateNewPost = true;
     this.isDisplayUserEditForm = false;
+    this.router.navigate(['/mon-compte/create-new-post']);
   }
 
   displayUserEditForm() {
     this.isDisplayPosts = false;
     this.isDisplayCreateNewPost = false;
     this.isDisplayUserEditForm = true;
+    this.router.navigate(['/mon-compte/edit']);
+
   }
 
 
   logout(){
-    console.log('logout');  
     this.userService.logout();  
    document.location.href="/"
   }
