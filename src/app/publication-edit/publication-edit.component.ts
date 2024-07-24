750ac50d8807bcdaff5d414a76e242d7066b1cb7
safiylm <121391404+safiylm@users.noreply.github.com>
2024-07-24 15:49:27 +0200

    improve edit post

--- src/app/publication-edit/publication-edit.component.ts
+++ src/app/publication-edit/publication-edit.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { PublicationsService } from '../../services/publication-service';
 import { Publication } from 'src/models/publication.model';
 import { FormGroup, FormControl } from "@angular/forms";
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-publication-edit',
@@ -11,24 +12,20 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class PublicationEditComponent {
 
-  id : string = "65cd023efb273094193ac038";
-  data?: Publication ;
-  post = new Publication( "", "","",[""], 0, "", );
+  id: string = "65cd023efb273094193ac038";
+  data?: Publication;
+  post = new Publication("", "", "", [""], 0, "",);
 
   constructor(private publicationService: PublicationsService, private route: ActivatedRoute) { }
-
 
   postEditForm = new FormGroup({
     title: new FormControl(""),
     body: new FormControl(""),
-    image: new FormControl(""),
-    video: new FormControl(""),
-    audio: new FormControl(""),
-   
+    image: new FormControl("")
   });
 
   getDataPost(): void {
-    this.publicationService.getPublicationById( this.id) 
+    this.publicationService.getPublicationById(this.id)
       .subscribe({
         next: (data) => {
           this.data = data;
@@ -37,35 +34,37 @@ export class PublicationEditComponent {
       });
   }
 
-  deletePost(){
-    if(confirm("Êtes-vous sur de vouloir supprimer la publication?")){
+  deletePost() {
+    if (confirm("Êtes-vous sur de vouloir supprimer la publication?")) {
       this.publicationService.deletePost(this.id)
     }
   }
 
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id')! ;
+    this.id = this.route.snapshot.paramMap.get('id')!;
     this.getDataPost();
+
+    this.publicationService.getPublicationById(this.id)
+      .pipe(first())
+      .subscribe(x => this.postEditForm.patchValue(x));
   }
 
-  onSubmit(){
+  onSubmit() {
     this.post._id = this.id;
-    this.post.title= this.postEditForm.value['title']?.toString() as string;
-    if( this.post.title==undefined){
-      this.post.title=this.data?.title.toString() as string;
-    }
+    this.post.title = this.data?.title.toString() as string;
+    this.post.body = this.data?.body.toString() as string;
+    this.post.userId = localStorage.getItem("userId") as string;
+    this.post.images[0] = this.data?.images[0].toString() as string;
 
-    this.post.body= this.postEditForm.value['body']?.toString() as string;
-    if( this.post.body==undefined){
-      this.post.body=this.data?.body.toString() as string;
-    }
+    if (this.postEditForm.controls['title'].valueChanges.subscribe.length != 0)
+      this.post.title = this.postEditForm.controls['title'].value as string
 
-    this.post.images[0]= this.postEditForm.value['image']?.toString() as string;
-    if( this.post.images[0]==undefined){
-      this.post.images[0]=this.data?.images.toString() as string;
-    }
+    if (this.postEditForm.controls['body'].valueChanges.subscribe.length != 0)
+      this.post.body = this.postEditForm.controls['body'].value as string
 
-    console.log(this.post);
+    if (this.postEditForm.controls['image'].value != "")
+      this.post.images[0] = this.postEditForm.controls['image'].value as string
+
     this.publicationService.editPost(this.post);
   }
 
