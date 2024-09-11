c5cfac8ab6a330f20ad19fa8da8cbc408cacf575
safiylm <121391404+safiylm@users.noreply.github.com>
2024-09-11 10:45:26 +0200

    Regle password + check if 2 pw is identique

--- src/app/auth-inscription-user/auth-inscription-user.component.ts
+++ src/app/auth-inscription-user/auth-inscription-user.component.ts
@@ -14,25 +14,75 @@ import { interval, scan, takeWhile } from 'rxjs';
 export class AuthInscriptionUserComponent implements OnInit {
   constructor(private userService: UserService) { }
 
+  isDisplayPassword !: boolean;
+  isDisplayPassword2 !: boolean;
   timeForRedirection$?: any;
-
+  firstPassword !: string;
+  secondPassword !: string;
+  is2PasswordIdentique !: boolean;
   displayToasts = false;
-
+  reglePasswordRespected !: boolean;
 
   inscriptionUserForm = new FormGroup({
     lastName: new FormControl("", [Validators.required]),
-    firstName: new FormControl("",[Validators.required]),
-    email: new FormControl("",[Validators.required]),
-    password: new FormControl("",[Validators.required]),
-    password2: new FormControl("",[Validators.required]),
-    phoneNo: new FormControl("",[Validators.required]),
-    photos_background: new FormControl("",[Validators.required]),
-    photos_profil: new FormControl("",[Validators.required])
+    firstName: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$')]),
+    password2: new FormControl("", [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$')]),
+    phoneNo: new FormControl("", [Validators.required]),
+    photos_background: new FormControl("", [Validators.required]),
+    photos_profil: new FormControl("", [Validators.required])
   });
-  user = new User("", "", "", "", "",0, "", "");
+  user = new User("", "", "", "", "", 0, "", "");
 
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.isDisplayPassword = false;
+    this.isDisplayPassword2 = false;
+    this.firstPassword = "";
+    this.secondPassword = "";
+    this.reglePasswordRespected = false;
+    this.is2PasswordIdentique = false;
+  }
+
+  toggleDisplayPassword() {
+    this.isDisplayPassword = !this.isDisplayPassword;
+  }
+
+
+  toggleDisplayPassword2() {
+    this.isDisplayPassword2 = !this.isDisplayPassword2;
+  }
+
+
+  getFirstPassword(event: any) {
+    this.firstPassword = event.target.value;
+   
+    if (this.firstPassword.length> 8 &&
+      this.firstPassword.match('[a-z]+')!= null  &&
+      this.firstPassword.match('[A-Z]+')!= null  &&
+      this.firstPassword.match('[0-9]+')!= null  &&
+      this.firstPassword.match('[!@#$%^&*]+')!= null 
+
+    ) {
+      this.reglePasswordRespected = true;
+    } else {
+      this.reglePasswordRespected = false;
+
+    }
+
+  }
+
+  getSecondPassword(event: any) {
+    this.secondPassword = event.target.value;
+    if (this.firstPassword === this.secondPassword) {
+      this.is2PasswordIdentique = true;
+    } else {
+      this.is2PasswordIdentique = false;
+
+    }
+  }
+
 
   onSubmit() {
     if (this.inscriptionUserForm.valid) {
@@ -44,24 +94,21 @@ export class AuthInscriptionUserComponent implements OnInit {
       this.user.photos_background = this.inscriptionUserForm.value['photos_background']?.toString() as string;
       this.user.photos_profil = this.inscriptionUserForm.value['photos_profil']?.toString() as string;
 
-     const resins = this.userService.pushNewUser(this.user)
-console.log(resins)
+      const resins = this.userService.pushNewUser(this.user)
+      console.log(resins)
       this.displayToasts = true;
 
       this.timeForRedirection$ = interval(1000).pipe(
         scan(acc => --acc, 10),
         takeWhile(x => x >= 0)
       );
-      
+
       setTimeout(() => {
         document.location.href = '/connexion'
       }, 10000)
 
     }
   }
-
-
-
 
 }
 
