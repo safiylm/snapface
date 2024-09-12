0205d8cab851caebc1732cd2a3a9c7fae6a3d040
safiylm <121391404+safiylm@users.noreply.github.com>
2024-09-12 15:06:43 +0200

    Update publication.route.js

--- backend/routes/publication.route.js
+++ backend/routes/publication.route.js
@@ -4,14 +4,11 @@ var router_publication = require("express").Router();
 
 // Create a new User
 router_publication.post("/api/publication/create", publication.create);
-
-
 router_publication.post("/api/publication/edit",  publication.edit );
 router_publication.post("/api/publication/delete",  publication.delete );
+
 router_publication.get("/api/publication", publication.findAll);
 router_publication.get("/api/publicationByUserId", publication.findAllPublicationByUserId);
-router_publication.get("/api/publicationByPostId", publication.findAllPublicationByPostId);
-
 router_publication.get("/api/publicationid", publication.findOneById);
 // /:id
 module.exports = router_publication;
