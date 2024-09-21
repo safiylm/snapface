7b02201c84c5ffbffa79947fb19a90d29e5d5e3d
safiylm <121391404+safiylm@users.noreply.github.com>
2024-09-21 16:20:43 +0200

    create post with x photos

--- src/app/publication-create/publication-create.component.ts
+++ src/app/publication-create/publication-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Publication } from '../../models/publication.model';
 import { FormGroup, FormControl } from "@angular/forms";
 import { PublicationsService } from '../../services/publication-service';
@@ -12,36 +12,51 @@ export class PublicationCreateComponent implements OnInit {
   @Input() userId !: string ;
   
   post = new Publication( "", "","",[''], 0, this.userId,);
-  array_image = [0,1];
+  array_image !: string[];
+  newimage !: string;
   postCreateForm = new FormGroup({
     title: new FormControl(""),
     body: new FormControl(""),
     image: new FormControl(""),
-    //video: new FormControl(""),
-    //audio: new FormControl(""),
-   
   });
+
+  @ViewChild("image") connexionInfo !: ElementRef;
+
+  
   constructor(private publicationsService: PublicationsService) { }
 
-  // addnewImages(){
-  //   this.array_image.push( this.array_image.length)
-  //   console.log(this.array_image)
-  // }
+  ngOnInit() {
+  }
+  ngAfterViewInit(){
+    this.array_image=[]
+    this.connexionInfo.nativeElement.innerText = "";
+
+  }
+
+  addNewImage(){
+    this.array_image.push( this.postCreateForm.value['image']?.toString() as string);
+    this.newimage= "";
+    this.connexionInfo.nativeElement.innerHTML = 
+    this.connexionInfo.nativeElement.innerHTML + " <img width='200px' height='auto' src=' " + 
+    this.array_image[ this.array_image.length-1] +"' alt='Nouvelle image n°"+ this.array_image.length +"'/>"
+  }
   
   onSubmit() {
     this.post.title= this.postCreateForm.value['title']?.toString() as string;
     this.post.body= this.postCreateForm.value['body']?.toString() as string;
-    this.post.images[0]= this.postCreateForm.value['image']?.toString() as string;
+    this.post.images= this.array_image as [string]; 
     this.post.userId = this.userId;
+    console.log(this.post);
+
+
     this.publicationsService.createNewPublication(this.post);
     setTimeout(() => {
       document.location.href= "/mon-compte"
     }, 2000);
+    
   }
 
-  ngOnInit() {
- 
-   }
+
 
 }
 
