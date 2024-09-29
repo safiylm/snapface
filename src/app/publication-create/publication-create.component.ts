7be6928dc23a8a457d65af361c09a3a61b79888a
safiylm <121391404+safiylm@users.noreply.github.com>
2024-09-29 16:46:15 +0200

    Ajouter plusieurs photos (dans creer une publication)

--- src/app/publication-create/publication-create.component.ts
+++ src/app/publication-create/publication-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Publication } from '../../models/publication.model';
 import { FormGroup, FormControl } from "@angular/forms";
 import { PublicationsService } from '../../services/publication-service';
@@ -8,10 +8,11 @@ import { PublicationsService } from '../../services/publication-service';
   templateUrl: './publication-create.component.html',
   styleUrls: ['./publication-create.component.scss']
 })
+
+
 export class PublicationCreateComponent implements OnInit {
-  @Input() userId !: string ;
-  
-  post = new Publication( "", "","",[''], 0, this.userId,);
+  @Input() userId !: string;
+  post = new Publication("", "", "", [''], 0, this.userId,);
   array_image !: string[];
   newimage !: string;
   postCreateForm = new FormGroup({
@@ -20,43 +21,37 @@ export class PublicationCreateComponent implements OnInit {
     image: new FormControl(""),
   });
 
-  @ViewChild("image") connexionInfo !: ElementRef;
-
-  
   constructor(private publicationsService: PublicationsService) { }
 
   ngOnInit() {
-  }
-  ngAfterViewInit(){
-    this.array_image=[]
-    this.connexionInfo.nativeElement.innerText = "";
-
+    this.array_image = []
   }
 
-  addNewImage(){
-    this.array_image.push( this.postCreateForm.value['image']?.toString() as string);
-    this.newimage= "";
-    this.connexionInfo.nativeElement.innerHTML = 
-    this.connexionInfo.nativeElement.innerHTML + " <img width='200px' height='auto' src=' " + 
-    this.array_image[ this.array_image.length-1] +"' alt='Nouvelle image n°"+ this.array_image.length +"'/>"
+  addNewImage() {
+    this.array_image.push(this.postCreateForm.value['image']?.toString() as string);
+    this.newimage = "";
   }
-  
+
+  deleteImage(nb: number) {
+    this.array_image = this.array_image.filter((item, i) => i !== nb)
+  }
+
   onSubmit() {
-    this.post.title= this.postCreateForm.value['title']?.toString() as string;
-    this.post.body= this.postCreateForm.value['body']?.toString() as string;
-    this.post.images= this.array_image as [string]; 
+    this.post.title = this.postCreateForm.value['title']?.toString() as string;
+    this.post.body = this.postCreateForm.value['body']?.toString() as string;
+    this.post.images = this.array_image as [string];
     this.post.userId = this.userId;
-    console.log(this.post);
-
 
     this.publicationsService.createNewPublication(this.post);
     setTimeout(() => {
-      document.location.href= "/mon-compte"
+      document.location.href = "/mon-compte"
     }, 2000);
-    
+
   }
 
-
+  get ArrayImage(){
+    return (this.array_image  )? this.array_image : null
+  }
 
 }
 
