1ec87bdd00b7b5589cddfa12c00b75ee9663e1b9
safiylm <121391404+safiylm@users.noreply.github.com>
2024-11-26 17:48:00 +0100

    lazy loading for comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,85 +1,87 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PublicationEditComponent } from './publication-edit/publication-edit.component';
-import { UserDataUpdateComponent } from './user-data-update/user-data-update.component';
-import { PublicationCreateComponent } from './publication-create/publication-create.component';
 import { AuthGuard } from './guards/auth.guard';
-import { MyAccountComponent } from './my-account/my-account.component';
-
 import { AllPublicationsResolverService } from 'src/services/resolver/all-publications-resolver-service';
 import { MyPublicationsResolverService } from 'src/services/resolver/my-publications-resolver-service';
 import { MyUserDataResolverService } from 'src/services/resolver/my-user-data-resolver-service';
 import { PublicationsByUserIdResolverService } from 'src/services/resolver/publications-by-userid-resolver-service';
 import { AllUsersResolverService } from 'src/services/resolver/all-users-resolver-service';
 import { UserDataResolverService } from 'src/services/resolver/user-data-resolver-service';
-
 import { formulaireDesactiveGuard } from './guards/formulaire-desactive.guard'
-import { PasswordEditComponent } from './password-edit/password-edit.component';
-import { UserAccountComponent } from './user-account/user-account.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: 'user/:id', 
-  //  loadComponent: import('./user-account/user-account.component'),
-  component: UserAccountComponent,
-
-    resolve: { publications: PublicationsByUserIdResolverService , 
-      user : UserDataResolverService }
+    path: 'user/:id',
+    loadComponent: () => import('./user-account/user-account.component')
+      .then(mod => mod.UserAccountComponent),
+    resolve: {
+      publications: PublicationsByUserIdResolverService,
+      user: UserDataResolverService
+    }
   },
   {
-    path: '', 
-     loadComponent :()=> import('./home/home.component')
-     .then(mod => mod.HomeComponent) , 
-    //component : HomeComponent,
-    resolve: { publications: AllPublicationsResolverService, 
-      users: AllUsersResolverService }
+    path: '',
+    loadComponent: () => import('./home/home.component')
+      .then(mod => mod.HomeComponent),
+    resolve: {
+      publications: AllPublicationsResolverService,
+      users: AllUsersResolverService
+    }
   },
-  { path: 'connexion', 
-    loadComponent:   () => import('./auth-connexion-user/auth-connexion-user.component')
-    .then(mod => mod.AuthConnexionUserComponent)                   
-   },
+  {
+    path: 'connexion',
+    loadComponent: () => import('./auth-connexion-user/auth-connexion-user.component')
+      .then(mod => mod.AuthConnexionUserComponent)
+  },
   {
     path: 'inscription',
-    loadComponent:   () => import('./auth-inscription-user/auth-inscription-user.component')
-    .then(mod => mod.AuthInscriptionUserComponent),   
+    loadComponent: () => import('./auth-inscription-user/auth-inscription-user.component')
+      .then(mod => mod.AuthInscriptionUserComponent),
     canDeactivate: [formulaireDesactiveGuard]
 
   },
   {
-    path: 'mon-compte', 
-    component: MyAccountComponent, 
+    path: 'mon-compte',
+    loadComponent: () => import('./my-account/my-account.component')
+      .then(mod => mod.MyAccountComponent),
     canActivate: [AuthGuard],
-    resolve: {publications: MyPublicationsResolverService, 
-      user : MyUserDataResolverService
+    resolve: {
+      publications: MyPublicationsResolverService,
+      user: MyUserDataResolverService
     },
+    // child route path
     children: [
       {
-        path: 'edit',  // child route path
+        path: 'edit',  
         title: "Edit data",
-        component: UserDataUpdateComponent,  // child route component that the router renders
-        canDeactivate: [formulaireDesactiveGuard], 
-       
+        loadComponent: () => import('./user-data-update/user-data-update.component')
+          .then(mod => mod.UserDataUpdateComponent),
+        canDeactivate: [formulaireDesactiveGuard],
+
       },
       {
-        path: 'create-new-post',  // child route path
+        path: 'create-new-post',
         title: "Create new post",
-        component: PublicationCreateComponent,  // child route component that the router renders
+        loadComponent: () => import("./publication-create/publication-create.component")
+          .then(mod => mod.PublicationCreateComponent),
         canDeactivate: [formulaireDesactiveGuard]
       },
     ]
   },
   {
     path: 'publication/edit/:id',
-    component: PublicationEditComponent,
     title: "Modifier sa publication",
+    loadChildren: () => import('./publication-edit/publication-edit.component')
+      .then(mod => mod.PublicationEditComponent),
     canDeactivate: [formulaireDesactiveGuard]
   },
 
   {
     path: 'user/password/edit',
-    component: PasswordEditComponent,
     title: "Modifier son mot de passe",
+    loadComponent: () => import("./password-edit/password-edit.component")
+      .then(mod => mod.PasswordEditComponent),
+
     canActivate: [AuthGuard],
   },
 
