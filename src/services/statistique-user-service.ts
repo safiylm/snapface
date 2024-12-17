9a39106628645468430e808ac13ed89034fd7d9b
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-17 22:44:42 +0100

    check total points

--- src/services/statistique-user-service.ts
+++ src/services/statistique-user-service.ts
@@ -87,5 +87,48 @@ export class StatistiqueUserService {
   }
 
 
+  checkTotalPoints(id: string): Observable<any> | void {
+
+    this.http.get<StatistiqueUser>("https://snapface.onrender.com/api/statistiqueUserByUserId?id=" + id)
+      .subscribe({
+        next: (data) => {
+
+          this.http.get<Publication[]>("https://snapface.onrender.com/api/publicationByUserId?id=" + id)
+            .subscribe({
+              next: (data1) => {
+                //console.log(data.totalPoints + " != " + data1)
+                let total = 0
+                let index =0
+                data1.forEach((value) => {
+
+                  this.http.get<InteractionSociale>("https://snapface.onrender.com/api/interactionSocialByPostId?id=" + value._id)
+                    .subscribe({
+                      next: (data2) => {
+                        total = data2.points + total
+                        index++
+                      }, complete: () => {
+                        console.log( + ". total " + total + "/ " + data1.length)
+
+                        if (index== data1.length)
+                          this.http
+                            .post<any>('http://localhost:4100/api/checkPoints'
+                              , { "id": id, "points": total },
+
+                            ).subscribe(data2 => {
+                              if (data2)
+                                console.log("Total posts in Statistique User update successful")
+                            })
+                      }
+                    })
+                })
+
+              }, error: (e) => console.error(e),
+              complete: () => console.log("TERMINEE")
+            })
+
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
 
