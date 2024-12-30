865f064a0eda7d9092994db688ca3527a48489ad
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-30 22:10:00 +0100

    change form reactive to template-driven

--- src/app/publication-edit/publication-edit.component.ts
+++ src/app/publication-edit/publication-edit.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PublicationsService } from '../../services/publication-service';
 import { Publication } from 'src/models/publication.model';
-import { FormGroup, FormControl, ReactiveFormsModule } from "@angular/forms";
-import { first } from 'rxjs';
+import { FormsModule } from "@angular/forms";
 import { Subscription } from 'rxjs';
 import { NgFor } from '@angular/common';
 
@@ -12,29 +11,21 @@ import { NgFor } from '@angular/common';
   selector: 'app-publication-edit',
   templateUrl: './publication-edit.component.html',
   styleUrls: ['./publication-edit.component.scss'],
-  imports:[ReactiveFormsModule, NgFor]
+  imports:[ NgFor, FormsModule]
 })
 export class PublicationEditComponent {
 
   id: string = "";
-  data?: Publication;
-  post = new Publication("", "", "", [""], 0, "",);
+  post!: Publication;
   subscription !: Subscription;
 
   constructor(private publicationService: PublicationsService, private route: ActivatedRoute) { }
-
-  postEditForm = new FormGroup({
-    title: new FormControl(""),
-    body: new FormControl(""),
-    image: new FormControl("")
-  });
-
 
   getDataPost(): void {
     this.subscription = this.publicationService.getPublicationById(this.id)
       .subscribe({
         next: (data) => {
-          this.data = data;
+          this.post = data;
         },
         error: (e) => console.error(e)
       });
@@ -42,13 +33,12 @@ export class PublicationEditComponent {
 
 
   deleteImage(nb: number) {
-    this.data!.images = this.data?.images.filter((item, i) => i !== nb) as [string]
+    this.post!.images = this.post?.images.filter((item, i) => i !== nb) as [string]
   }
 
 
-  addNewImage() {
-    this.data?.images.push(this.postEditForm.value['image']?.toString() as string);
-    this.postEditForm.controls['image'].setValue("")
+  addNewImage(e : string ) {
+    this.post?.images.push(e);
   }
 
 
@@ -62,32 +52,14 @@ export class PublicationEditComponent {
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id')!;
     this.getDataPost();
-    this.publicationService.getPublicationById(this.id)
-      .pipe(first())
-      .subscribe(x => this.postEditForm.patchValue(x));
   }
 
 
   onSubmit() {
-    this.post._id = this.id;
-    this.post.title = this.data?.title.toString() as string;
-    this.post.body = this.data?.body.toString() as string;
-    this.post.userId = localStorage.getItem("userId") as string;
-    this.post.images = this.data?.images as [string];
-
-    if (this.postEditForm.controls['title'].valueChanges.subscribe.length != 0)
-      this.post.title = this.postEditForm.controls['title'].value as string
-
-    if (this.postEditForm.controls['body'].valueChanges.subscribe.length != 0)
-      this.post.body = this.postEditForm.controls['body'].value as string
-
-
-    this.publicationService.editPost(this.post);
+    this.publicationService.editPost(this.post!);
     setTimeout(() => {
       document.location.href = "/mon-compte"
     }, 2000);
-
-
   }
 
   ngOnDestroy() {
