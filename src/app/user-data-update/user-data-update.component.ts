883a219cccb6997e59620dbaf30ea173ea72ec32
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-30 21:48:59 +0100

    change form reactive to template-driven

--- src/app/user-data-update/user-data-update.component.ts
+++ src/app/user-data-update/user-data-update.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserService } from '../../services/user-service'
 import { User } from '../../models/user.model'
-import { FormGroup, FormControl, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { Subscription } from 'rxjs';
 import { HeaderComponent } from '../header/header.component';
 
@@ -11,28 +11,15 @@ import { HeaderComponent } from '../header/header.component';
   selector: 'app-user-data-update',
   templateUrl: './user-data-update.component.html',
   styleUrls: ['./user-data-update.component.scss'], 
-  imports:[ReactiveFormsModule, HeaderComponent]
+  imports:[ReactiveFormsModule, HeaderComponent, FormsModule],
 })
 
 export class UserDataUpdateComponent implements OnInit {
 
-  userId = localStorage.getItem("userId")?.toString() as string;
-
   constructor(private userService: UserService) { }
 
-  user?: User;
-  user2 = new User(localStorage.getItem("userId")?.toString() as string, "", "", "", "", 0, "", "");
+  user !: User;
   subscription !: Subscription;
-
-  updateUserForm = new FormGroup({
-    lastName: new FormControl(""),
-    firstName: new FormControl(),
-    email: new FormControl(""),
-    password: new FormControl(""),
-    phoneNo: new FormControl(""),
-    photos_background: new FormControl(""),
-    photos_profil: new FormControl("")
-  });
 
 
   retrieveUser(): void {
@@ -56,29 +43,8 @@ export class UserDataUpdateComponent implements OnInit {
 
 
   onSubmit() {
-    this.user2._id = localStorage.getItem("userId") as string;
-    this.user2.lastName = this.user?.lastName.toString() as string;
-    this.user2.firstName = this.user?.firstName.toString() as string;
-    this.user2.email = this.user?.email.toString() as string;
-    this.user2.password = this.user?.password.toString() as string;
-    this.user2.photos_background = this.user?.photos_background.toString() as string;
-    this.user2.phoneNo = Number(this.user?.phoneNo.toString());
-    this.user2.photos_profil = this.user?.photos_profil.toString() as string;
-
-    if (this.updateUserForm.controls['lastName'].value != "") {
-      this.user2.lastName = this.updateUserForm.controls['lastName'].value as string
-    }
-    if (this.updateUserForm.controls['firstName'].value != "") {
-      this.user2.firstName = this.updateUserForm.controls['firstName'].value as string
-    }
-    if (this.updateUserForm.controls['photos_background'].value != "") {
-      this.user2.photos_background = this.updateUserForm.controls['photos_background'].value as string
-    }
-    if (this.updateUserForm.controls['photos_profil'].value != "") {
-      this.user2.photos_profil = this.updateUserForm.controls['photos_profil'].value as string
-    }
-    this.userService.updateUser(this.user2);
-
+    console.log(this.user!)
+    this.userService.updateUser(this.user!);
   }
   
 }
