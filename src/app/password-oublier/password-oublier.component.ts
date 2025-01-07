3c19434a449259cac79e62410a7dd99cc5a0b9e9
safiylm <121391404+safiylm@users.noreply.github.com>
2025-01-07 09:48:52 +0100

    password oublie component termine

---
+++ src/app/password-oublier/password-oublier.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user-service'
+import * as bcrypt from "bcryptjs";
+import { NgClass, NgIf, NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-password-oublier',
+  templateUrl: './password-oublier.component.html',
+  styleUrls: ['./password-oublier.component.scss'],
+  standalone:true, 
+  imports:[FormsModule, NgStyle, NgIf]
+})
+export class PasswordOublierComponent {
+
+  id: string = localStorage.getItem('userId')?.toString() as string;
+
+  isDisplayPassword !: boolean;
+  isDisplayPassword2 !: boolean;
+  newpassword !: string;
+  newpassword2 !: string;
+  reglePasswordRespected !: boolean;
+  is2PasswordIdentique !: boolean;
+  resultat =""
+
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
+
+
+  toggleDisplayPassword(nb: number) {
+    if(nb==1)
+    this.isDisplayPassword = !this.isDisplayPassword;
+    if(nb==2)   
+    this.isDisplayPassword2 = !this.isDisplayPassword2;
+
+  }
+
+  getFirstPassword(event: any) {
+    this.newpassword = event.target.value;
+
+    if (this.newpassword.length > 8 &&
+      this.newpassword.match('[a-z]+') != null &&
+      this.newpassword.match('[A-Z]+') != null &&
+      this.newpassword.match('[0-9]+') != null &&
+      this.newpassword.match('[!@#$%^&*]+') != null
+
+    ) {
+      this.reglePasswordRespected = true;
+    } else {
+      this.reglePasswordRespected = false;
+    }
+  }
+
+  getSecondPassword(event: any) {
+    this.newpassword2 = event.target.value;
+    if (this.newpassword === this.newpassword2) {
+      this.is2PasswordIdentique = true;
+    } else {
+      this.is2PasswordIdentique = false;
+    }
+  }
+
+
+
+  ngOnInit() {
+
+    this.isDisplayPassword = false;
+    this.isDisplayPassword2 = false;
+    this.newpassword = "";
+    this.newpassword2 = "";
+    this.reglePasswordRespected = false;
+    this.is2PasswordIdentique = false;
+  }
+
+  onSubmit() {
+    const salt = bcrypt.genSaltSync(10);    
+    if (this.newpassword === this.newpassword2) {
+      
+      this.userService.editPassword(this.id, bcrypt.hashSync(this.newpassword, salt))
+      .subscribe(data => {
+        if (data) {
+        this.resultat = "Votre mot de passe a été modifié avec succès!"
+        }else{
+          this.resultat = "Erreur veuillez réessayer!"
+        }
+      })
+    }
+  }
+}
