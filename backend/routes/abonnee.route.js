695226e9076a0cfd93c0dc1de7b1a6c683398a1e
safiylm <121391404+safiylm@users.noreply.github.com>
2025-01-14 15:06:26 +0100

    Update abonnee.route.js

--- backend/routes/abonnee.route.js
+++ backend/routes/abonnee.route.js
@@ -5,10 +5,12 @@ var router_abonnees = require("express").Router();
 // Create a new User
 //router_publication.post("/api/publication", interactionsociales.create);
 router_abonnees.post("/api/abonnees/create", abonnees.create);
-router_abonnees.post("/api/abonnees/abonneeAdd", abonnees.abonneeAdd);
-router_abonnees.post("/api/abonnees/abonneeRemove", abonnees.abonneeRemove);
+router_abonnees.post("/api/abonnees/add", abonnees.abonneeAdd);
+router_abonnees.post("/api/abonnees/remove", abonnees.abonneeRemove);
 
 router_abonnees.get("/api/abonneesbyUserId", abonnees.findByUserId);
+router_abonnees.get("/api/post/myabonnement", abonnees.findPostOfMyAbonnement);
+
 
 module.exports = router_abonnees;
 
