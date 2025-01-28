9dfe9485227255e617e6a5bff8e0b4c013b06363
safiylm <121391404+safiylm@users.noreply.github.com>
2025-01-28 20:39:16 +0100

    header test render

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -11,10 +11,21 @@ describe('HeaderComponent', () => {
              
       fixture = TestBed.createComponent(HeaderComponent);
       component = fixture.componentInstance;
+      fixture.detectChanges();
     });
 
     it('should create', () => {
       expect(component).toBeDefined();
     });
+
+    it('test render', ()=>{
+     
+     const compiled = fixture.nativeElement as HTMLElement;
+     const items = compiled.querySelectorAll('a');
+     expect(items.length).toBe(2);
+     expect(items[0].textContent).toBe('Snapface');
+     expect(items[1].textContent).toBe('Connexion');
+  
+    })
 
 })
