641877fcd58a66bbee4c5c4bc7f72ba00b68fbeb
safiylm <121391404+safiylm@users.noreply.github.com>
2025-02-05 09:35:57 +0100

    add button follow

--- src/app/auteur-in-post-or-commentaire/auteur-in-post-or-commentaire.component.ts
+++ src/app/auteur-in-post-or-commentaire/auteur-in-post-or-commentaire.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, Input } from '@angular/core';
 import {User} from '../../models/user.model';
 import {UserService} from '../../services/user-service';
 import { Subscription } from 'rxjs';
+import { ButtonFollowComponent } from "../button-follow/button-follow.component";
 
 @Component({
   standalone:true, 
   selector: 'app-auteur-in-post-or-commentaire',
   templateUrl: './auteur-in-post-or-commentaire.component.html',
-  styleUrls: ['./auteur-in-post-or-commentaire.component.scss']
+  styleUrls: ['./auteur-in-post-or-commentaire.component.scss'],
+  imports: [ButtonFollowComponent]
 })
 
 export class AuteurInPostOrCommentaireComponent  implements OnInit {
@@ -16,6 +18,7 @@ export class AuteurInPostOrCommentaireComponent  implements OnInit {
   @Input() id !: string ;
   subscription !: Subscription;
   user !: User;
+  isMe=false;
 
   constructor(private UserService: UserService) { }
 
@@ -32,6 +35,8 @@ export class AuteurInPostOrCommentaireComponent  implements OnInit {
 
   ngOnInit() {
    this.retrieveUser()
+   if(localStorage.getItem("userId")==this.id)
+    this.isMe=true;
   }
 
   get PhotoProfil(){
