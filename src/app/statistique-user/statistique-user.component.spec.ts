4efd903d298382aa4b0a31ba680659f743686f0c
safiylm <121391404+safiylm@users.noreply.github.com>
2025-02-18 17:27:07 +0100

    test statistique user comp

--- src/app/statistique-user/statistique-user.component.spec.ts
+++ src/app/statistique-user/statistique-user.component.spec.ts
@@ -8,6 +8,9 @@ describe('StatistiqueUserComponent', () => {
   let component: StatistiqueUserComponent;
   let fixture: ComponentFixture<StatistiqueUserComponent>;
   let httpTestingController: HttpTestingController;
+  let followsNB !: HTMLElement;
+  let pointsTotal !: HTMLElement;
+  let postsTotal !: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,7 +21,11 @@ describe('StatistiqueUserComponent', () => {
     fixture = TestBed.createComponent(StatistiqueUserComponent);
     component = fixture.componentInstance;
     component.id = "66e9219abe68cdd15907399e";
+    component.statistiqueUser=new StatistiqueUser( "66e9219abe68cdd15907399f","66e9219abe68cdd15907399e", 1, 3, 7);
     fixture.detectChanges();
+    followsNB = fixture.nativeElement.querySelector("#btn-followers")
+    pointsTotal = fixture.nativeElement.querySelector("#pointsTotal")
+    postsTotal = fixture.nativeElement.querySelector("#postsTotal")
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
@@ -37,12 +44,27 @@ describe('StatistiqueUserComponent', () => {
     // Simuler une requête HTTP
     const req = httpTestingController.expectOne
       ('https://snapface.onrender.com/api/statistiqueUserByUserId?id=66e9219abe68cdd15907399e');
-    expect(req.request.method).toBe('GET'); // Vérifie le type de requête
+  
+      expect(req.request.method).toBe('GET'); // Vérifie le type de requête
     req.flush(mockData); // Envoie une réponse simulée
 
     // Vérifie que les données sont assignées correctement
     expect(component.statistiqueUser).toEqual(mockData);
 
+  });
+
+
+  it('should display followers', () => {
+    expect(followsNB.textContent).toEqual(" Followers "+component.statistiqueUser.followers);
+  });
+
+
+  it('should display total of points', () => {
+    expect(pointsTotal.textContent).toEqual(" Points totales "+component.statistiqueUser.totalPoints);
+  });
+
+  it('should display total of posts ', () => {
+    expect(postsTotal.textContent).toEqual(" Post totales "+component.statistiqueUser.totalPosts);
   });
 
 })
