3805387667762eb1da0618c063bd999e9e84e843
safiylm <121391404+safiylm@users.noreply.github.com>
2025-02-24 10:44:03 +0100

    test

---
+++ src/app/commentaire-create/commentaire-create.component.spec.ts
@@ -0,0 +1,82 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommentaireCreateComponent } from './commentaire-create.component';
+import { NgIf } from '@angular/common';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CommentaireService } from 'src/services/commentaire-service';
+import { Commentaire } from 'src/models/commentaire.model';
+import { of } from 'rxjs';
+
+describe('Commentaire Create Component', () => {
+  let component: CommentaireCreateComponent;
+  let fixture: ComponentFixture<CommentaireCreateComponent>;
+  let httpTestingController: HttpTestingController;
+  let service: CommentaireService;
+  
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      providers: [
+        CommentaireService,
+      ],
+      imports: [NgIf, HttpClientTestingModule],
+
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(CommentaireCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(CommentaireService);
+   component.id="662eb417c2fd9ad3238d752e"
+    component.commentaire = new Commentaire(
+      "6697ffa585ac11e40dccc044",
+      "La mer est un espace de rigueur et de liberté. Victor Hugo",
+      1721237413177,
+      "662eb2a1c2fd9ad3238d7528",
+      "662eb417c2fd9ad3238d752e"
+    );
+
+  });
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  it('init ', () => {
+    expect(component.id).toBeDefined() // postId
+    expect(component.result).toBeDefined()
+    //expect(component.commentaire).toBeDefined()
+
+  })
+
+
+
+  it('should create comment', () => {
+
+    // Arrange
+    const data = new Commentaire(
+      "6697ffa585ac11e40dccc044",
+      "Test Com Réussi",
+      1721237413177,
+      "662eb2a1c2fd9ad3238d7528",
+      "662eb417c2fd9ad3238d752e"
+    );
+
+    spyOn(component, 'createNewComment')
+    component.commentaire = data;
+
+    spyOn(service, 'addNewCommentaire').and
+      .returnValue(of(data));
+
+    // Act
+    component.createNewComment();
+
+    // Assert
+    expect(component.createNewComment).toHaveBeenCalled();
+  });
+
+
+
+})
--- src/app/commentaire/commentaire.component.spec.ts
+++ src/app/commentaire/commentaire.component.spec.ts
@@ -3,26 +3,37 @@ import { CommentaireComponent } from './commentaire.component';
 import { NgIf } from '@angular/common';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CommentaireService } from 'src/services/commentaire-service';
+import { Commentaire } from 'src/models/commentaire.model';
+import { of } from 'rxjs';
 
 describe('CommentaireComponent', () => {
   let component: CommentaireComponent;
   let fixture: ComponentFixture<CommentaireComponent>;
   let httpTestingController: HttpTestingController;
-
+  let service: CommentaireService;
+  
   beforeEach(() => {
 
     TestBed.configureTestingModule({
-       providers: [
-              CommentaireService,
-            ],
-      imports: [ NgIf, HttpClientTestingModule],
-      
+      providers: [
+        CommentaireService,
+      ],
+      imports: [NgIf, HttpClientTestingModule],
+
     }).compileComponents()
 
     fixture = TestBed.createComponent(CommentaireComponent);
-    component = fixture.componentInstance; 
+    component = fixture.componentInstance;
     fixture.detectChanges();
     httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(CommentaireService);
+    component.commentaire = new Commentaire(
+      "6697ffa585ac11e40dccc044",
+      "La mer est un espace de rigueur et de liberté. Victor Hugo",
+      1721237413177,
+      "662eb2a1c2fd9ad3238d7528",
+      "662eb417c2fd9ad3238d752e"
+    );
 
   });
 
@@ -31,5 +42,70 @@ describe('CommentaireComponent', () => {
     expect(component).toBeTruthy();
   });
 
+
+  it('init ', () => {
+    expect(component.isMyComment).toBeDefined()
+    expect(component.isDisplayingForm).toBeDefined()
+  })
+
+
+  it('open & close form editting comment  ', () => {
+
+    expect(component.isDisplayingForm).toEqual(false)
+    spyOn(component, 'showFormEditComment')
+    component.showFormEditComment();
+    expect(component.showFormEditComment).toHaveBeenCalled()
+    // expect(component.isDisplayingForm).toEqual(true)
+  })
+
+
+  it('should edit comment', () => {
+
+    // Arrange
+    const data = new Commentaire(
+      "6697ffa585ac11e40dccc044",
+      "La mer est un espace de rigueur et de liberté. Victor Hugo",
+      1721237413177,
+      "662eb2a1c2fd9ad3238d7528",
+      "662eb417c2fd9ad3238d752e"
+    );
+
+    spyOn(component, 'editComment')
+    component.commentaire = data;
+
+    spyOn(service, 'updateCommentaire').and
+      .returnValue(of(data));
+
+    // Act
+    component.editComment();
+
+    // Assert
+    expect(component.editComment).toHaveBeenCalled();
+  });
+
+
+  it('should delete comment', () => {
+
+    // Arrange
+    const data = new Commentaire(
+      "6697ffa585ac11e40dccc044",
+      "La mer est un espace de rigueur et de liberté. Victor Hugo",
+      1721237413177,
+      "662eb2a1c2fd9ad3238d7528",
+      "662eb417c2fd9ad3238d752e"
+    );
+
+    spyOn(component, 'deleteComment')
+    component.commentaire = data;
+
+    spyOn(service, 'deleteCommentaire').and
+      .returnValue(of(data));
+
+    // Act
+    component.deleteComment();
+
+    // Assert
+    expect(component.deleteComment).toHaveBeenCalled();
+  });
 
 })
