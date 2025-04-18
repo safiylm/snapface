416f24ee92b186e6e5c163bf6019b3d2be000f47
safiylm <121391404+safiylm@users.noreply.github.com>
2025-04-18 18:44:12 +0200

    chat prive send & receive msg with socket

--- backend/chat.js
+++ backend/chat.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:4200", // Autorise Angular à se connecter
@@ -14,33 +15,11 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 
-  console.log("Un utilisateur est connecté");
-
   socket.on("publicMessage", (msg) => {
     console.log("Message reçu :", msg);
     io.emit("publicMessage", msg); // Diffuse le message à tous
   });
-/*
-  socket.on("privateMessage", (msg) => {
-    io.emit("privateMessage", msg); // Diffuse le message à tous
-  });
-  */
 
-  socket.on('privateMessage', async ({ sender, receiver, conversationId, text }) => {
-      //try {
-        const response = await axios.post('http://localhost:4100/message/create', {
-          sender,
-          conversationId,
-          text
-         })
-         const savedMessage = response.data;
-         console.log(savedMessage);
-      //   io.to(receiver).emit('receiveprivateMessage', savedMessage)
-         io.emit('receiveprivateMessage', savedMessage);
-     /*  } catch (error) {
-         console.error('Erreur lors de l’enregistrement du message', error);
-       }*/
-  });
 
   /*
   Les rooms permettent d'envoyer des messages uniquement à certains utilisateurs au lieu de diffuser à tous. Cela est utile pour : ✅ Un chat privé (envoyer un message à un utilisateur spécifique).
@@ -48,12 +27,27 @@ io.on("connection", (socket) => {
 ✅ Un support en ligne (un agent parle à un client spécifique).
   */
 
-  socket.on('joinRoom', (userId) => {
-    socket.join(userId);
-    console.log("userId")
+  socket.on('privateMessage', async (data) => {
+
+    try {
+      const response = await axios.post('http://localhost:4100/message/create', {
+        sender: data['sender'],
+        conversationId: data['conversationId'],
+        text: data['text']
+      })
+      const savedMessage = response.data;
+
+      if (savedMessage['acknowledged'] == true) 
+      io.emit('newMessage', data); // broadcast to all
+    } catch (error) {
+      console.error('Erreur lors de l’enregistrement du message', error);
+    }
+
   });
 
-
+  socket.on('joinRoom', (userId) => {
+    socket.join(userId);
+  });
 
   socket.on("disconnect", () => {
     console.log("Utilisateur déconnecté");
