91fcdf3bc6551ed775e19805122c1a9e0cdadf00
safiylm <121391404+safiylm@users.noreply.github.com>
2025-04-27 23:42:45 +0200

    new component : list of post scroll like instagram

---
+++ src/app/post/publication-list-scroll/publication-list-scroll.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, Input } from '@angular/core';
+import { Publication } from '../../../models/publication.model';
+import { ActivatedRoute } from '@angular/router';
+import { PublicationComponent } from '../publication/publication.component';
+import { NgIf, NgFor } from '@angular/common';
+import { UsersListComponent } from "../../user/users-list/users-list.component";
+
+@Component({
+  standalone: true,
+  selector: 'app-publication-list-scroll',
+  templateUrl: './publication-list-scroll.component.html',
+  styleUrls: ['./publication-list-scroll.component.scss'],
+  imports: [PublicationComponent, NgFor, NgIf, UsersListComponent]
+
+})
+export class PublicationListScrollComponent {
+
+  publications!: Publication[];
+  constructor(route: ActivatedRoute) {
+    this.publications = route.snapshot.data['publications']
+  }
+
+
+
+}
