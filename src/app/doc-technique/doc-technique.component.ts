0608248bf80648252df317b4e85d3e62af157fa6
safiylm <121391404+safiylm@users.noreply.github.com>
2025-06-05 09:41:22 +0200

    doc technique

---
+++ src/app/doc-technique/doc-technique.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-doc-technique',
+  templateUrl: './doc-technique.component.html',
+  styleUrls: ['./doc-technique.component.scss']
+})
+export class DocTechniqueComponent {
+
+}
