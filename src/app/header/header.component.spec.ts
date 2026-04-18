c08e6b53f5fca5c97eceb7a229c0c553c5b382fa
safiylm <121391404+safiylm@users.noreply.github.com>
2026-04-18 20:27:39 +0200

    test for header component

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,31 +1,48 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-    let component: HeaderComponent;
-    let fixture : ComponentFixture<HeaderComponent>;
-    
-    beforeEach( () => {
-      TestBed.configureTestingModule({
-      }).compileComponents()
-             
-      fixture = TestBed.createComponent(HeaderComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
-    it('should create', () => {
-      expect(component).toBeDefined();
-    });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    }).compileComponents()
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('test render', ()=>{
-     
-     const compiled = fixture.nativeElement as HTMLElement;
-     const items = compiled.querySelectorAll('a');
-     expect(items.length).toBe(2);
-     expect(items[0].textContent).toBe('Snapface');
-     expect(items[1].textContent).toBe('Connexion');
-  
-    })
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('test render when user is not connected', () => {
+    component.isLoggedIn = false;
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const items = compiled.querySelectorAll('a');
+    expect(items.length).toBe(4);
+    expect(items[0].textContent).toBe('Snapface');
+    expect(items[1].textContent).toBe('Accueil');
+    expect(items[2].textContent).toBe('Chercher');
+    expect(items[3].textContent).toBe('Connexion');
+
+  })
+
+
+  it('test render when user is connected', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const items = compiled.querySelectorAll('a');
+    expect(items.length).toBe(5);
+    expect(items[0].textContent).toBe('Snapface');
+    expect(items[1].textContent).toBe('Accueil');
+    expect(items[2].textContent).toBe('Chercher');
+
+  })
 
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,7 @@
 import { NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostListener, OnInit, signal } from '@angular/core';
+import {  Component, HostListener, OnInit, signal } from '@angular/core';
 import { AvatarUserComponent } from "./avatar-user/avatar-user.component";
 import { ListFollowRequestComponent } from "../user/list-follow-request/list-follow-request.component";
-import { MatExpansionModule } from '@angular/material/expansion';
 import { UserService } from 'src/services/user-service';
 import { Router } from '@angular/router';
 import { ChatPriveService } from 'src/services/chatprive.service';
@@ -13,13 +12,11 @@ import { ChatPriveService } from 'src/services/chatprive.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   imports: [
-    NgIf, 
+    NgIf,
     AvatarUserComponent, AvatarUserComponent,
     ListFollowRequestComponent,
-    // MatAccordion, 
-    MatExpansionModule
+
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class HeaderComponent implements OnInit {
@@ -28,32 +25,32 @@ export class HeaderComponent implements OnInit {
   isLoggedIn: boolean = false;
   nbConversationWithNewMessages: any;
 
-  constructor( private userService: UserService,
-    private route: Router, protected chatService :ChatPriveService
+  constructor(private userService: UserService,
+    private route: Router, protected chatService: ChatPriveService
   ) {
   }
 
   ngOnInit() {
-    if (JSON.parse(localStorage.getItem('userconnected')?.toString() as string).userId != "") {
+    this.isLoggedIn = false;
+
+    if (localStorage.getItem('userconnected')!= null) {
       this.isLoggedIn = true;
-    } else {
-      this.isLoggedIn = false;
     }
 
-     this.chatService.getUsersWeHaveConversation(localStorage.getItem('userId')?.toString() as string)
-       .subscribe(
-         (data) => {
-           for (let conv of data) {
-             this.chatService.getNewMessagesByConversationId(conv._id)
-               .subscribe(
-                 (dataa) => {
-                   if (dataa.length > 0) {
-                     this.nbConversationWithNewMessages = this.nbConversationWithNewMessages + 1;
-                   }
-                 }
-               )
-           }
-         })
+    this.chatService.getUsersWeHaveConversation(localStorage.getItem('userId')?.toString() as string)
+      .subscribe(
+        (data) => {
+          for (let conv of data) {
+            this.chatService.getNewMessagesByConversationId(conv._id)
+              .subscribe(
+                (dataa) => {
+                  if (dataa.length > 0) {
+                    this.nbConversationWithNewMessages = this.nbConversationWithNewMessages + 1;
+                  }
+                }
+              )
+          }
+        })
   }
 
   logout() {
@@ -62,7 +59,7 @@ export class HeaderComponent implements OnInit {
         if (data == "ok") {
           localStorage.setItem('userconnected', '');
           localStorage.removeItem('userconnected')
-          location.href =''
+          location.href = ''
         }
       })
   }
